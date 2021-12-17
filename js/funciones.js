8d079836bb638e4defe1a00650b07552d63edcc6
CamiloEspinosaVeraInfomedia <camilo.espinosa@infomediaservice.com>
2021-12-17 18:48:24 -0500

    cambios boleteria presentacion

--- js/funciones.js
+++ js/funciones.js
@@ -1,22 +1,56 @@
-function tomarImagenPorSeccion(div,nombre) {
+function printImg() {
+	pwin = window.open(document.getElementById("BoletaDescarga").src,"_blank");
+	pwin.onload = function () {window.print();}
+  }
 
+  function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+	 if ((new Date().getTime() - start) > milliseconds) {
+	  break;
+	 }
+	}
+   }
+   
+   function demo() {
+	 console.log('Taking a break...');
+	 sleep(2000);
+	 console.log('Two second later');
+   }
+   
+   
+
+function tomarImagenPorSeccion(div,nombre) {
+	var img='';
 	html2canvas(document.querySelector("#" + div)).then(canvas => {
 		var img = canvas.toDataURL();
 		console.log(img);
-		base = "img=" + img + "&nombre=" + nombre;
 
-		$.ajax({
-			type:"POST",
-			url:"procesos/crearImagenes.php",
-			data:base,
-			success:function(respuesta) {	
-				respuesta = parseInt(respuesta);
-				if (respuesta > 0) {
-					alert("Imagen creada con exito!");
-				} else {
-					alert("No se pudo crear la imagen :(");
-				}
-			}
-		});
+		$("#divqr").html('<div id="divimagen"><img id="BoletaDescarga" src=" '+img+'" width="250" height="60" ></div><br>')
+		//$("#divqr2").append('<a download="boleta.jpg" href="'+img+'">Descargar Boleta</a>')
+		
+
+		 
 	});	
+
+	 
+}
+
+
+function tomarImagenPorSeccion2(div,nombre) {
+	
+	html2canvas(document.querySelector("#" + div)).then(canvas => {
+		var img = canvas.toDataURL();
+		console.log(img);
+
+		 
+		$("#divqr2").append('<a download="boleta.jpg" href="'+img+'">Descargar Boleta</a>')
+
+		$("#generar").hide();
+		
+
+		 
+	});	
+
+	 
 }
