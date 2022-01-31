b6b5500a5326736dd8a23cbc3776f684f067bcb0
AlfonsoR Infomedia <alfonso.atencio@infomediaservice.com>
2022-01-31 16:29:07 -0500

    Consumiendo api get atraccion

--- ajax/ajxRequest.php
+++ ajax/ajxRequest.php
@@ -18,29 +18,39 @@
             $token = $llamar_token->generarToken();
             //print_r($token);
             //echo "</br>";
-            //print_r($token->accessToken->token);
+            print_r($token->accessToken->token);
             //print_r($token->contentToken->token);
             //print_r($token->refreshToken->token);
             //
             if ($token->id != '') {
                 //
-                $token = $token->mensaje;
-                //
-                //Armando array para recibir datos de 
-                $array = ['t'=>"getInfoCliente", 'documento'=>$documento, 'token'=>$token];
-                $array = http_build_query($array, '', '&');
+                $headers = array();
+                //$headers = ['Authorization: Bearer '.$token->accessToken->token];
+                $headers[] = 'Authorization: Bearer '.$token->accessToken->token;
+                $headers[] = 'Content-Type: application/json';
                 //
                 //Enviamos datos para recibir respuesta de 
-                $ch = curl_init('https://');
-                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                curl_setopt($ch, CURLOPT_POSTFIELDS, $array);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_POST, 1);
+                $ch = curl_init('http://20.44.111.223:80/api/boleteria/atraccion'); 
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");  
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers); 
                 //
                 $result = curl_exec($ch);
                 curl_close($ch);
-                $result = json_decode($result);                      
+                //$result = json_decode($result);
+                echo "<br><br>";
+                print_r($result); exit;                      
                 //
+                /*var settings3 = {
+					"url": "http://20.44.111.223:80/api/boleteria/atracciones",
+					//"url": "http://20.44.111.223:80/api/boleteria/tiposBoleta?incluirImagen=true",
+					"method": "GET",
+					"async": false,
+					"timeout": 0,
+					"headers": {
+						"Authorization": "Bearer "+localStorage.getItem('accessToken')
+					},
+				}; */
                 if ($result->error == 0) {/* **************************************** */
                         
                 } else {                
