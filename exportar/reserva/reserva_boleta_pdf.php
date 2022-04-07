f5aea4ca0df6a584aaca9c91119ece97601ceea5
CamiloEspinosaVera1003 <espinosaveracamilo@gmail.com>
2022-04-07 08:38:00 -0500

    cambios quitar numero locker

--- exportar/reserva/reserva_boleta_pdf.php
+++ exportar/reserva/reserva_boleta_pdf.php
@@ -225,7 +225,7 @@
 
             QRcode::png($key->id, $codesDir.$codeFile, "H", 3); 
 
-            $html.=' <tr> <td width="100%" style="text-align:center;" >'.$key->tipoBoleta->nombre.' <br> Boleta valida para: <br> '.$fecha_reserva.' <br> '.$nombre.' '.$apellido.' <br> # Locker: '.$key->visitante->reservaCasilla->casilla->id.' - Cod: '.$key->visitante->codigo.' <br> # Boleta: '.$rest.' <img class="img-thumbnail "  width="140" height="140" src="'.$codesDir.$codeFile.'" /> <br> <span class="El-cdigo-QR-te-servir-para-identificarte-e-ingresar-al-parque">
+            $html.=' <tr> <td width="100%" style="text-align:center;" >'.$key->tipoBoleta->nombre.' <br> Boleta valida para: <br> '.$fecha_reserva.' <br> '.$nombre.' '.$apellido.' <br>  Codigo: '.$key->visitante->codigo.' <br> # Boleta: '.$rest.' <img class="img-thumbnail "  width="140" height="140" src="'.$codesDir.$codeFile.'" /> <br> <span class="El-cdigo-QR-te-servir-para-identificarte-e-ingresar-al-parque">
             El código QR te servirá para identificarte e ingresar al parque
             </span> </td>  <td style="text-align:center;" width="40%"></td> </tr>  ';
        
